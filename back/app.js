23483777bc788d3e949a38e197353ac35b8b73f9
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2015-03-22 03:49:58 +0200

    Replaces spawn with exec and removes socket.io-logger

--- back/app.js
+++ back/app.js
@@ -1,46 +1,50 @@
-var io = require('socket.io');
+var io = require('socket.io')();
 var fs = require('fs');
 var path = require('path');
-var spawn = require('child_process').spawn;
+var exec = require('child_process').exec;
 var temp = require('temp');
-var options = {stream: fs.createWriteStream('/var/log/sock_events.log', {flags: 'a'})};
-var logger = require('socket.io-logger')(options);
-var stream = require('stream');
 var util = require('util');
+var rimraf = require('rimraf');
 
-io.use(logger);
 var conflCheckerPath = path.join(__dirname, 'conflchecker', 'conflcheck.pl');
 
 io.on('connection', function (socket) {
-
-    socket.on('check confluence', function (fileData) {
+    socket.on("check confluence", function(fileData) {
         temp.mkdir('conflcheck', function (err, dirPath) {
             if (err) {
                 socket.emit('result', {success: false, message: "Server failed to create temp directory" });
                 throw err;
             }
+            console.log("created dir", dirPath);
             var testFilePath = path.join(dirPath, 'test.pl');
             fs.writeFile(testFilePath, fileData, function(err) {
                 if (err) {
                    socket.emit('result', {success: false, message: "Server failed to write test file"});
                    throw err;
                 }
-                var s = new stream.Readable();
-                s._read = function noop() {};
-                s.push(util.format('consult(%s).\ncheck_confluence(%s), show_critical_pairs(%s).', conflCheckerPath,
-                    testFilePath, testFilePath));
-                s.push(null);
-                var spawnOptions = {
+
+                var input = util.format('consult(\'%s\').\ncheck_confluence(\'%s\'), show_critical_pairs(\'%s\').\n', conflCheckerPath,
+                    testFilePath, testFilePath);
+                var command = util.format('echo "%s" | swipl', input)
+                var options = {
+                    encoding: 'utf8',
+                    timeout: 0,
+                    maxBuffer: 200*1024,
+                    killSignal: 'SIGTERM',
                     cwd: dirPath,
-                    stdio: [s, 'pipe', 'pipe']
+                    env: process.env 
                 };
-                var swipl= spawn('swipl', [], spawnOptions);
-                swipl.on('close', function(code) {
-                    socket.emit('result', {success: code == 0, message: swipl.stdout.read()});
-                })
-            });
-        });
-    });
+                exec(command, options, function(err, stdout, stderr) {
+                    var success = true;
+                    if(err) {
+                        succes = false;
+                    }
+                    socket.emit('result', {success: success, message: stdout});
+                    rimraf.sync(dirPath);
+                }); //exec             
+            }); // write file
+        }); // mkdir
+    }); // cehck confluence event
 });
 
-io.listen(8080);
+io.listen(8081);
